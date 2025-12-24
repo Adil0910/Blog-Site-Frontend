50e6480293e490cd04926a4624cc5e09b9c13d88
Adil0910 <aadilehtesham63@gmail.com>
2025-12-24 12:19:24 +0530

    commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -15,9 +15,9 @@ const Footer = () => {
           <h4>Quick Links</h4>
           <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="/blog">Blogs</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/footer">Contact</a></li>
+            <li><a href="/">Blogs</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Contact</a></li>
           </ul>
         </div>
 
